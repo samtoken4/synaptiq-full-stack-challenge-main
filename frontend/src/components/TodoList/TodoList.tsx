04ec31a3c57e469b2537ef9309470ee7752e33b8
sam <sam@metatokenlabs.com>
2024-01-30 17:58:34 -0800

    update todos list

--- frontend/src/components/TodoList/TodoList.tsx
+++ frontend/src/components/TodoList/TodoList.tsx
@@ -1,43 +1,133 @@
-import { Box, Typography, Checkbox, FormControlLabel, Paper } from "@mui/material";
-import { useEffect, useMemo, useState } from "react";
+import { Box, Checkbox, FormControlLabel, Paper, Button } from "@mui/material";
+import { useMemo, useState } from "react";
 
-interface TodoListProps {
-
+type TodoItem = {
+    id: number,
+    name: string,
+    completed: boolean,
 }
+const defaultTodos: TodoItem[] = [{ id: 1, name: 'Pay electric bill', completed: false }, { id: 2, name: 'Walk the dog', completed: false }];
+
+enum FilterStatus {
+    ALL = 'all',
+    ACTIVE = 'active',
+    COMPLETE = 'complete'
+}
+
 const TodoList = () => {
+    const defaultTodosCompletedState = { [defaultTodos[0].id]: defaultTodos[0], [defaultTodos[1].id]: defaultTodos[1] };
 
-    const defaultTodos = ['Pay electric bill', 'Walk the dog'];
-
-    const defaultTodosCheckedState = { [defaultTodos[0]]: false, [defaultTodos[1]]: false }
+    const [todosCompletedState, setTodosCompletedState] = useState<Record<number, TodoItem>>(defaultTodosCompletedState);
     const [newTodo, setNewTodo] = useState('');
+    const [newTodoId, setNewTodoId] = useState(3);
 
-    const [todosState, setTodosState] = useState<Record<string, boolean>>(defaultTodosCheckedState)
+    const [filter, setFilter] = useState(FilterStatus.ALL);
+    const filteredTodos = useMemo(() => Object.fromEntries(Object.entries(todosCompletedState).filter(([_itemId, todoItem]) => {
+        if (filter === FilterStatus.ACTIVE) {
+            return !todoItem.completed;
+        } else if (filter === FilterStatus.COMPLETE) {
+            return todoItem.completed;
+        } else {
+            return true;
+        }
+    })), [todosCompletedState, filter]);
+
+    const currentTodoList = Object.entries(filteredTodos);
+
+    const completedTodos = (filter === FilterStatus.COMPLETE && filteredTodos) || Object.fromEntries(Object.entries(todosCompletedState).filter(([_itemId, todoItem]) => {
+        return todoItem.completed;
+    }));
+
+    console.log('completedTodos', completedTodos, Object.keys(completedTodos));
+
+    const buttonStyle = {
+        padding: "4px",
+        margin: '8px 12px',
+        color: "white",
+        "&:hover": {
+            backgroundColor: "#1188e1",
+        },
+    };
+
+    const renderFilterButtons = () => (<>
+        <Button
+            sx={{
+                backgroundColor: filter === FilterStatus.ALL ? "#1188e1" : "#85949f",
+                ...buttonStyle,
+            }}
+            onClick={() => setFilter(FilterStatus.ALL)}
+        >
+            All
+        </Button>
+        <Button
+            sx={{
+                backgroundColor: filter === FilterStatus.ACTIVE ? "#1188e1" : "#85949f",
+                ...buttonStyle,
+            }}
+            onClick={() => setFilter(FilterStatus.ACTIVE)}
+        >
+            Active
+        </Button>
+        <Button
+            sx={{
+                backgroundColor: filter === FilterStatus.COMPLETE ? "#1188e1" : "#85949f",
+                ...buttonStyle,
+            }}
+            onClick={() => setFilter(FilterStatus.COMPLETE)}
+        >
+            Completed
+        </Button>
+    </>)
 
     const addTodo = () => {
         if (newTodo.trim() !== '') {
-            setTodosState({ ...todosState, [newTodo]: false });
+            const todoItem = {
+                id: newTodoId,
+                name: newTodo,
+                completed: false,
+            };
+
+            setTodosCompletedState({ ...todosCompletedState, [todoItem.id]: todoItem });
+
+            setNewTodoId(newTodoId + 1);
             setNewTodo('');
         }
     };
 
-    const renderTodos = () => (<Paper sx={{ paddingLeft: 2, display: 'flex', flexDirection: 'column', width: '130px', margin: '12px 48px' }}>
-        {defaultTodos.map((todo) => (
-            <li>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={todosState[todo]}
-                            onChange={() => {
-                                setTodosState({ ...todosState, [todo]: !todosState[todo] });
-                            }}
-                        />
-                    }
-                    label={todo}
-                />
-            </li>
-        )
-        )}
-    </Paper>)
+    const handleClearCompleted = () => {
+        const activeTodos = Object.fromEntries(Object.entries(todosCompletedState).filter(([_itemId, todoItem]) => {
+            return !todoItem.completed;
+        }));
+        setTodosCompletedState(activeTodos);
+    }
+
+    const renderTodos = () => (<>
+        {(currentTodoList).map(([itmId, todoItem]) => {
+            const parsedItemId = parseInt(itmId);
+
+            return (
+                <li style={{ listStyleType: 'none', minWidth: '150px' }} className={todosCompletedState[parsedItemId].completed ? 'completed' : ''} >
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={todosCompletedState[parsedItemId].completed}
+                                onChange={() => {
+                                    setTodosCompletedState({ ...todosCompletedState, [parsedItemId]: { ...todosCompletedState[parsedItemId], completed: !todosCompletedState[parsedItemId].completed } });
+                                }}
+                            />
+                        }
+                        label={todoItem.name}
+                    />
+                </li>
+            )
+        })}
+    </>);
+
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            addTodo();
+        }
+    };
 
     return (
         <Box textAlign='center' className='todo-list' mt={4}>
@@ -46,9 +136,27 @@ const TodoList = () => {
                 value={newTodo}
                 data-test={'new-todo'}
                 onChange={(e) => setNewTodo(e.target.value)}
+                onKeyDown={handleKeyPress}
                 placeholder="Add a new to-do"
             />
             <button onClick={addTodo}>Add</button>
+
+            {renderFilterButtons()}
+
+            {Object.keys(completedTodos).length ?
+                <Button
+                    sx={{
+                        backgroundColor: "white",
+                        ...buttonStyle,
+                        color: "#b7325f",
+                        fontWeight: 600,
+                    }}
+                    onClick={handleClearCompleted}
+                >
+                    Clear completed
+                </Button>
+                : null}
+
             {renderTodos()}
         </Box>
     );
